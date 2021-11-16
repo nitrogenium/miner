f8b627c0239d42b555980510390a62c1ba9c64cf
Andy Adams <liquid.nitrogenium@gmail.com>
2021-11-16 11:58:24 +0500

    wip

--- update.sh
+++ update.sh
@@ -2,15 +2,13 @@
 
 version=20211115-1
 
-# mkdir -p miners/18/nv
-# mkdir -p miners/18/amd
-# mkdir -p miners/20/nv
-# mkdir -p miners/20/amd
+mkdir -p miners/18/nv
+mkdir -p miners/18/amd
+
 
 wget https://github.com/tontechio/pow-miner-gpu/releases/download/$version/minertools-cuda-ubuntu-18.04-x86-64.tar.gz -P miners
-# wget https://github.com/tontechio/pow-miner-gpu/releases/download/$version/minertools-cuda-ubuntu-20.04-x86-64.tar.gz -P miners
 wget https://github.com/tontechio/pow-miner-gpu/releases/download/$version/minertools-opencl-ubuntu-18.04-x86-64.tar.gz -P miners
-# wget https://github.com/tontechio/pow-miner-gpu/releases/download/$version/minertools-opencl-ubuntu-20.04-x86-64.tar.gz -P miners
+
 
 curl -L -O https://newton-blockchain.github.io/global.config.json
 
